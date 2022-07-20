d784cab23777666c2a1d89f138c7079d811f8187
FlayoGamerz112 <109572902+FlayoGamerz112@users.noreply.github.com>
2022-07-20 18:45:45 +0530

    Update song.py

--- plugins/admemes/song.py
+++ plugins/admemes/song.py
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**💥 Søɳʛ 🎸 Uƥɭøɗɘɗ 💿 Ɓy✌\n🔊 [𝕋𝕖𝕔𝕙𝕟𝕠 𝕄𝕚𝕟𝕕𝕫 💞 ṀṳṠḭḉ](https://t.me/technomindzchat) 🌷 ...**'
+        rep = '**💥 Søɳʛ 🎸 Uƥɭøɗɘɗ 💿 Ɓy✌\n🔊 [FLAYO 💞 ṀṳṠḭḉ](https://t.me/allnewmoviessupport) 🌷 ...**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
